b6210fe7e62573dcaa7eef984cfb7049e51f1bff
Ken Sanders <Kenneth.Sanders@acxiom.com>
2025-09-17 14:15:18 -0500

    Updated CSS margin, added more b64 image support to flatten

--- flattenHTML.py
+++ flattenHTML.py
@@ -18,15 +18,25 @@ args = args.parse_args()
 assert Path(args.input).exists(), f"Input file {args.input} does not exist."
 assert Path(args.input).suffix == ".html", "Input file must be an HTML file."
 
+base_path = Path(args.input).parent
+
 with Path(args.input).open("r", encoding="utf-8") as f:
     soup = BeautifulSoup(f, "html.parser")
 
 for img in soup.find_all("img"):
-    src = unquote(img.get("src", ""))
-    if src.endswith(".svg") and os.path.exists(src):
+    src = Path(base_path)/unquote(img.get("src", ""))
+    if src.suffix in (".svg",):
         with Path(src).open("rb") as svg_file:
             encoded = base64.b64encode(svg_file.read()).decode("utf-8")
             img["src"] = f"data:image/svg+xml;base64,{encoded}"
+    elif src.suffix in (".png",):
+        with Path(src).open("rb") as png_file:
+            encoded = base64.b64encode(png_file.read()).decode("utf-8")
+            img["src"] = f"data:image/png;base64,{encoded}"
+    elif src.suffix in (".jpg",".jpeg"):
+        with Path(src).open("rb") as jpg_file:
+            encoded = base64.b64encode(jpg_file.read()).decode("utf-8")
+            img["src"] = f"data:image/jpeg;base64,{encoded}"
 
 with Path(args.output).open("w", encoding="utf-8") as f:
     f.write(str(soup))
